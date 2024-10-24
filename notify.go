1e3c963adbfa26bbed578b1a47e11800f149574f
Dave Smith <dave.smith@chainguard.dev>
2024-10-24 12:07:07 -0400

    Update notify.go
    increases maxDupeTime from 3 days to 4
    
    Signed-off-by: Dave Smith <dave.smith@chainguard.dev>

--- notify.go
+++ notify.go
@@ -22,7 +22,7 @@ var (
 	nonAlpha       = regexp.MustCompile(`\W+`)
 
 	// Suppress duplicate messages within this time period
-	maxDupeTime = time.Hour * 24 * 6
+	maxDupeTime = time.Hour * 24 * 4
 	// Follow-up to threads that are within this time period
 	relationTime    = time.Hour
 	maxRelationTime = time.Hour * 4
