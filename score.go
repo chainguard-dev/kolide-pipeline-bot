43fa022f54cc31d08d29d0aededf577d5b778111
Thomas Stromberg <t+github@chainguard.dev>
2025-02-25 17:07:35 -0500

    Improve prompting

--- score.go
+++ score.go
@@ -47,11 +47,27 @@ func scoreRow(ctx context.Context, model *genai.GenerativeModel, row *DecoratedR
 	}
 
 	prompt := genai.Text(fmt.Sprintf(`
-		This is the JSON output from Kolide, based on an osquery rule named %q that is part of osquery-defense-kit.
+		This is the JSON output from Kolide, based on an osquery rule named %q that is part of osquery-defense-kit. These JSON rows represent exceptions from the normal baseline in our environment.
 
-		The 'Row' struct is the content returned by the query. If an element starts with p0, it is the child process the alert is centered on. If an element starts with p1_, that is the parent of the process we alerted on. If the element begins with p2_, it is the grandparent process. The grandparent process launches the parent process, which launches the child process.
-		The 'Decorations' struct contains information about the machine the query was run on.
-		The 'VirusTotal' struct contains any additional information that VirusTotal discovered about the process or hosts returned by the query. If the VirusTotal struct is empty, it may mean that VirusTotal wasn't available.
+		Almost all of the time these rows are just benign changes to our environment when people install new software, but we're also very cautious about subtle nation-state attackers getting a foothold in our environment.
+
+		The 'Row' struct is the content returned by the query:
+		- If an element starts with p0, it is the child process the alert is centered on.
+		- If an element starts with p1_, that is the parent of the process we alerted on.
+		- If the element begins with p2_, it is the grandparent process.
+		- The grandparent process launches the parent process, which launches the child process.
+		- If an element is named s_auth or authority, the binary is signed and approved by Apple. It could still be malware, particularly if it is signed by a rare application signer.
+		- If an element is named s_id or identifier, that is the name of the binary that was signed and approved by Apple
+		- Unsigned programs running longer than a day should be regarded with more suspicion than otherwise.
+
+		The 'Decorations' struct contains information about the machine the query was run on. Think of it as a host information struct.
+
+		The 'VirusTotal' struct contains additional information that VirusTotal discovered about the process or IP addresses
+	    returned by the query.
+		- If the VirusTotal struct is empty, it may mean that VirusTotal wasn't available.
+		- A verdict of "undetected_no_opinion" means that VirusTotal did not know about this hash. This is much more common on Linux. It may be a program that the user built for themselves locally.
+		- If a program is harmless_and_known, the tool itself probably benign, but even benign tools can be misused.
+		- If the remote_address is related to GitHub or Microsoft, it probably isn't as suspicious as it looks.
 
 		Return a single-word verdict of if the behavior and process tree described by this is benign, undetermined, suspicious, or malicious, followed by a colon and a 5-10 word summary of the row.
 
